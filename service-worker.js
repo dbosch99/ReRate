bebcc664b66e81639d62d8e8c5d18a55a7349a44
Davide Boschetti <bdavide1999@gmail.com>
2026-04-21 00:04:53 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // === cambia SOLO questa riga per forzare un refresh completo ===
-const CACHE = 'rerate-20-04-2026-1';
+const CACHE = 'rerate-20-04-2026-2';
 // ==============================================================================
 
 const ASSETS = [
